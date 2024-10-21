bb28c3793d4b84c7b3852c9ab62bf61921e1f709
Al_Iraq1 <152442043+q0so@users.noreply.github.com>
2024-10-21 13:17:12 +0300

    Create resetup.sh

---
+++ resetup.sh
@@ -0,0 +1,11 @@
+rm -rf zphisher &&
+rm -rf overload &&
+rm -rf change-username-tiktok &&
+rm -rf sherlock &&
+rm -rf SpamWa &&
+rm -rf TBomb &&
+rm -rf wifite2 &&
+rm -rf Numbers-counter &&
+rm -rf JS_URL_Finder &&
+rm -rf JS_domainToIP &&
+bash setup.sh
