aed370d1c94451f6c40892ed65f31e743e2954ad
Al_Iraq1 <152442043+q0so@users.noreply.github.com>
2025-01-22 16:31:22 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ pkg install git -y &&
 python3 -m pip install requests &&
 pkg install python -y &&
 pkg install python3 -y &&
+git clone https://github.com/palahsu/DDoS-Ripper.git &&
 git clone --depth=1 https://github.com/htr-tech/zphisher.git &&
 git clone https://github.com/7zx/overload.git &&
 git clone https://github.com/0xf15/change-username-tiktok.git &&
